4db90e9923566b27d92718f248918656ad7a52ee
patzineubi <105806213+patzineubi@users.noreply.github.com>
2023-04-13 20:52:05 +0200

    feat: - adding the pop-up window for opponent cards (click for appearance)

--- app/src/main/java/at/moritzmusel/gwent/GameViewActivity.java
+++ app/src/main/java/at/moritzmusel/gwent/GameViewActivity.java
@@ -9,12 +9,18 @@ import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.DragEvent;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,14 +30,33 @@ import java.util.concurrent.ThreadLocalRandom;
 public class GameViewActivity extends AppCompatActivity {
 
     private ArrayList<ImageView> imageViewList;
-    private LinearLayout ll;
+    private LinearLayout ll, llOpponent;
     private String msg;
     private android.widget.LinearLayout.LayoutParams layoutParams;
+    private Button buttonOpponentCards;
+    private int counterOpponentView = 0;
+    private PopupWindow popupWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_view);
+
+        buttonOpponentCards = (Button) findViewById(R.id.buttonOpponentCards);
+        buttonOpponentCards.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(counterOpponentView % 2 == 0) {
+                    buttonOpponentCards.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_baseline_keyboard_arrow_up_24) );
+                    onButtonShowPopupWindowClick(getWindow().getDecorView().getRootView());
+                    counterOpponentView++;
+                } else {
+                    buttonOpponentCards.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_baseline_keyboard_arrow_down_24) );
+                    popupWindow.dismiss();
+                    counterOpponentView++;
+                }
+            }
+        });
 
         ll = (LinearLayout) findViewById(R.id.linearLayoutMainCardsDeck);
         imageViewList = new ArrayList<>();
@@ -107,6 +132,10 @@ public class GameViewActivity extends AppCompatActivity {
         }
     }
 
+    private void setOpponentCardDeck() {
+
+    }
+
     /**
      * Card is currently randomly chosen.
      * @param image
@@ -136,4 +165,54 @@ public class GameViewActivity extends AppCompatActivity {
         }
     }
 
+    private void setImageFromAssetForOpponent(ImageView image) {
+        Bitmap bitmap = ((BitmapDrawable) getDrawable(R.drawable.card_deck_back)).getBitmap();
+        Drawable dr = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap, 50, 70, true));
+        image.setImageDrawable(dr);
+    }
+
+    /**
+     * Pop-op windows of cards from opponent.
+     * At the moment the number of cards is randomly chosen.
+     * @param view
+     */
+    public void onButtonShowPopupWindowClick(View view) {
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_window_opponent, null);
+        llOpponent = popupView.findViewById(R.id.linearLayoutMainCardsDeckOpponent);
+
+        int size = ThreadLocalRandom.current().nextInt(1,  6);
+        System.out.println(size);
+
+        if(size == 1) {
+            ImageView im = new ImageView(view.getContext());
+            im.setPadding(50, 50, 50, 50);
+            setImageFromAssetForOpponent(im);
+            llOpponent.addView(im);
+        } else {
+            for(int i = 0; i < size; i++) {
+                ImageView im = new ImageView(view.getContext());
+                if (i == 0) {
+                    im.setPadding(50, 50, 12, 50);
+                } else if (i == size-1) {
+                    im.setPadding(12, 50, 50, 50);
+                } else {
+                    im.setPadding(12, 50, 12, 50);
+                }
+                setImageFromAssetForOpponent(im);
+                llOpponent.addView(im);
+            }
+        }
+
+        // create the popup window
+        int width = ConstraintLayout.LayoutParams.WRAP_CONTENT;
+        int height = ConstraintLayout.LayoutParams.WRAP_CONTENT;
+
+        popupWindow = new PopupWindow(popupView, width, height, false);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+    }
 }
