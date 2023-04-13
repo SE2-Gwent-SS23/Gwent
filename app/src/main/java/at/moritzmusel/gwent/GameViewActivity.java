d6f4cb2837e2fd72cd0610651dafc0f7416a0974
patzineubi <105806213+patzineubi@users.noreply.github.com>
2023-04-13 13:55:53 +0200

    feat: setting background image for game view

---
+++ app/src/main/java/at/moritzmusel/gwent/GameViewActivity.java
@@ -0,0 +1,15 @@
+package at.moritzmusel.gwent;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameViewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_view);
+
+        
+    }
+}
