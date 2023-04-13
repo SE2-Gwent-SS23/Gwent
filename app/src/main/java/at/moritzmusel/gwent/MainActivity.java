72c004e837934e8b0f97f80286373fcf1f02ed30
patzineubi <105806213+patzineubi@users.noreply.github.com>
2023-04-13 16:10:48 +0200

    feat: - changing properties of lanes - adding ImageView of card stack - adding number of round textfield

--- app/src/main/java/at/moritzmusel/gwent/MainActivity.java
+++ app/src/main/java/at/moritzmusel/gwent/MainActivity.java
@@ -2,6 +2,7 @@ package at.moritzmusel.gwent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.game_view);
+
+        //Intent gameViewIntent = new Intent(MainActivity.this, GameViewActivity.class);
+        //startActivity(gameViewIntent);
     }
 }
