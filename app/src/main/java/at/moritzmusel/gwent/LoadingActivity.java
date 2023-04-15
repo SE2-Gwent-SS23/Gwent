97bb1474dcba52b112e9c9906475b1c8952d1ac9
Markus Ulbing <m1ulbing@edu.aau.at>
2023-04-15 14:20:21 +0200

    feat: - changed progressbar to horizontal style

--- app/src/main/java/at/moritzmusel/gwent/LoadingActivity.java
+++ app/src/main/java/at/moritzmusel/gwent/LoadingActivity.java
@@ -4,19 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.ProgressBar;
 
 public class LoadingActivity extends AppCompatActivity {
 
+    int progressValue = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
-        /*Intent MenuIntent = new Intent(LoadingActivity.this, MenuActivity.class);
-        startActivity(MenuIntent);*/
 
         ProgressBar progressBar = findViewById(R.id.progressBar); // initiate the progress bar
         progressBar.setVisibility(View.VISIBLE);
+        progressValue = progressBar.getProgress();
+        progressBar.setMax(100); // 100 maximum value for the progress value
+        progressBar.setProgress(20); // 10 default progress value for the progress bar
+
+        //placeholder
+        new CountDownTimer(5000, 1000) {
+
+            @Override
+            public void onTick(long l) {
+                progressValue += 20;
+                progressBar.setProgress(progressValue, true);
+            }
+
+            @Override
+            public void onFinish() {
+                /*Intent MenuIntent = new Intent(LoadingActivity.this, MenuActivity.class);
+                startActivity(MenuIntent);*/
+            }
+        }.start();
     }
 }
