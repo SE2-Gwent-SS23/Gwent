c4172db4febe0f90c9bf6324b9e91bb222a6b27a
Markus Ulbing <m1ulbing@edu.aau.at>
2023-04-17 10:37:17 +0200

    feat: - basic design

---
+++ app/src/main/java/at/moritzmusel/gwent/MainMenuActivity.java
@@ -0,0 +1,14 @@
+package at.moritzmusel.gwent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
