eeb70edc804b80d87f6271417dc22592e9720420
patzineubi <105806213+patzineubi@users.noreply.github.com>
2023-05-01 20:26:21 +0200

    fix: - moved test files to at(test) folder

--- app/src/test/java/at/moritzmusel/gwent/GameViewActivityTest.java
+++ app/src/test/java/at/moritzmusel/gwent/GameViewActivityTest.java
@@ -1,10 +1,15 @@
 package at.moritzmusel.gwent;
 
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 
 import at.moritzmusel.gwent.ui.GameViewActivity;
 
@@ -19,5 +24,12 @@ public class GameViewActivityTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test_navigateToGameView() {
+        Espresso.onView(ViewMatchers.withId(R.id.imageViewWelcomeScreen)).perform((ViewActions.click()));
+        Espresso.onView(ViewMatchers.withId(R.id.textView)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        Espresso.onView(ViewMatchers.withId(R.id.progressBar)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
     }
 }
