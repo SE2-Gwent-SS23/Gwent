af2d3e7a78a8eb18f5e45ac05c81b95f1abfdef7
Markus Ulbing <m1ulbing@edu.aau.at>
2023-05-15 11:30:29 +0200

    refactoring and more tests

--- app/src/androidTest/java/at/moritzmusel/gwent/MainMenuActivityTest.java
+++ app/src/androidTest/java/at/moritzmusel/gwent/MainMenuActivityTest.java
@@ -3,15 +3,20 @@ package at.moritzmusel.gwent;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import androidx.test.espresso.Root;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
+import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
 
 public class MainMenuActivityTest {
     @Rule
@@ -46,12 +51,29 @@ public class MainMenuActivityTest {
     public void test_popup_menu() {
         //loading
         try {
-            Thread.sleep(5000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
         onView(withId(R.id.imageView_play)).perform(click());
-        //onView(withId(R.menu.popup_menu)).check(matches(isDisplayed()));
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withText("Host")).inRoot(isPopupWindow()).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //check if Activity changed
+        onView(withId(R.id.iv_profile_pic_opponent)).check(matches(isDisplayed()));
+    }
+    public static Matcher<Root> isPopupWindow() {
+        return isPlatformPopup();
     }
 }
