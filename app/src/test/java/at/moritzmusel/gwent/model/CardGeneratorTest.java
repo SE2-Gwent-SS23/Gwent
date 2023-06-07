f0ac530eb989b0a3c20de35a017a3fbace4a20d8
patzineubi <105806213+patzineubi@users.noreply.github.com>
2023-06-07 11:21:41 +0200

    feat: - adding JUnitTests for CardGenerator class

--- app/src/test/java/at/moritzmusel/gwent/model/CardGeneratorTest.java
+++ app/src/test/java/at/moritzmusel/gwent/model/CardGeneratorTest.java
@@ -1,72 +1,116 @@
 package at.moritzmusel.gwent.model;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import static at.moritzmusel.gwent.model.Ability.scorch_s;
-import static at.moritzmusel.gwent.model.Type.scoiatael;
+import android.content.Context;
+import android.content.res.AssetManager;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import at.moritzmusel.gwent.ui.GameViewActivity;
+import at.moritzmusel.gwent.network.data.GameState;
 
 public class CardGeneratorTest {
+    private JSONObject jsonObject;
+    private JSONArray jsonArray;
+    private List<Card> cardList;
 
     private CardGenerator cardGenerator;
-    @Mock
-    private JSONObject jsonObject;
+    private AssetManager assetManager;
+    private Context context;
 
     @BeforeEach
     void init() {
-        cardGenerator = CardGenerator.getInstance();
+        context = mock(Context.class);
+        assetManager = mock(AssetManager.class);
+        cardGenerator = new CardGenerator(context);
+        cardList = new ArrayList<>();
+        jsonObject = new JSONObject();
+        jsonArray = new JSONArray();
+        when(context.getAssets()).thenReturn(assetManager);
     }
 
     @Test
-    void testCreateNewCardListWithJSONObject() throws JSONException, IOException {
-        String jsonString = "{\n" +
-                "  \"cards\": [\n" +
-                "    {\n" +
-                "      \"name\": \"Mysterious Elf\",\n" +
-                "      \"type\": \"neutral\",\n" +
-                "      \"row\": \"close\",\n" +
-                "      \"strength\": \"0\",\n" +
-                "      \"ability\": \"hero spy\",\n" +
-                "      \"filename\": \"mysterious_elf\",\n" +
-                "      \"count\": \"1\",\n" +
-                "      \"flavor_txt\": \"You humans have... unusual tastes.\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"name\": \"Decoy\",\n" +
-                "      \"type\": \"special\",\n" +
-                "      \"row\": \"none\",\n" +
-                "      \"strength\": \"0\",\n" +
-                "      \"ability\": \"decoy\",\n" +
-                "      \"filename\": \"decoy\",\n" +
-                "      \"count\": \"3\",\n" +
-                "      \"flavor_txt\": \"When you run out of peasants, decoys also make decent arrow fodder.\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"name\": \"Biting Frost\",\n" +
-                "      \"type\": \"weather\",\n" +
-                "      \"row\": \"none\",\n" +
-                "      \"strength\": \"0\",\n" +
-                "      \"ability\": \"frost\",\n" +
-                "      \"filename\": \"frost\",\n" +
-                "      \"count\": \"3\",\n" +
-                "      \"flavor_txt\": \"Best part about frost - bodies of the fallen don't rot so quickly.\"\n" +
-                "    }\n" +
-                "  ]\n" +
-                "}";
+    public void testGetInstance() {
+        CardGenerator cardGeneratorInstance1 = CardGenerator.getInstance();
+        CardGenerator cardGeneratorInstance2 = CardGenerator.getInstance();
 
-        jsonObject = new JSONObject(jsonString);
-        /*TODO assert content */
-        //assertEquals(jsonString, cardGenerator.fillAllCardsIntoList(jsonObject));
+        assertNotNull(cardGeneratorInstance1);
+        assertNotNull(cardGeneratorInstance2);
+        assertEquals(cardGeneratorInstance1, cardGeneratorInstance2);
     }
+
+    @Test
+    public void testLoadCardJSONFromAssetException() throws IOException {
+        when(assetManager.open("cards.json")).thenThrow(new IOException("File not found"));
+        String jsonString = cardGenerator.loadCardJSONFromAsset();
+
+        assertNull(jsonString);
+    }
+
+    @Test
+    public void testFillAllCardsIntoList() throws JSONException, IOException {
+        jsonObject.put("cards", jsonArray);
+
+        String name = "Decoy";
+        int strength = 5;
+        int count = 2;
+        String flavor_txt = "Test text";
+        String filename = "monsters_arachas.jpg";
+
+        JSONObject newCardObj = new JSONObject();
+        newCardObj.put("row", "NONE");
+        newCardObj.put("name", name);
+        newCardObj.put("type", "REALMS");
+        newCardObj.put("strength", String.valueOf(strength));
+        newCardObj.put("count", String.valueOf(count));
+        newCardObj.put("flavor_txt", flavor_txt);
+        newCardObj.put("filename", filename);
+        newCardObj.put("ability", "RAIN");
+
+        jsonArray.put(newCardObj);
+
+        Card newCard = new Card();
+        newCard.setName(name);
+        newCard.changeType("REALMS");
+        newCard.setStrength(strength);
+        newCard.setCount(count);
+        newCard.setFlavor_txt(flavor_txt);
+        newCard.setFilename(filename);
+        newCard.changeRow("NONE");
+        newCard.changeAbility("RAIN");
+        cardList.add(newCard);
+
+        assertEquals(cardList.toString(), cardGenerator.fillAllCardsIntoList(jsonObject).toString());
+    }
+
+    @Test
+    public void testInitMyHandCards() {
+        GameState gameState = new GameState(0, 0, 0, false);
+        List<Card> myHand = new ArrayList<>();
+        Card card1 = new Card();
+        card1.setCount(2);
+        Card card2 = new Card();
+        card2.setCount(0);
+        myHand.add(card1);
+        myHand.add(card2);
+
+        gameState.setMyHand(myHand);
+
+        assertEquals(2, myHand.size());
+        assertEquals(card1, myHand.get(0));
+        assertEquals(card2, myHand.get(1));
+    }
+
 }
