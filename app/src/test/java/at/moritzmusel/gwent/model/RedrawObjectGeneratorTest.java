0896cba8adb32852b523af27afb9180fa6b40f43
patzineubi <105806213+patzineubi@users.noreply.github.com>
2023-06-08 09:44:37 +0200

    feat: - adding JUnitTests for RedrawObjectGenerator class update

--- app/src/test/java/at/moritzmusel/gwent/model/RedrawObjectGeneratorTest.java
+++ app/src/test/java/at/moritzmusel/gwent/model/RedrawObjectGeneratorTest.java
@@ -26,32 +26,20 @@ import at.moritzmusel.gwent.network.data.GameState;
 
 public class RedrawObjectGeneratorTest {
 
-    private GameState gameState;
     private RedrawObjectGenerator redrawObjectGenerator;
-    private CardGenerator cardGenerator;
-    private Context context;
-    private JSONObject jsonObject;
-    private JSONArray jsonArray;
-    private List<Card> cardList;
 
     @BeforeEach
     void init() {
         redrawObjectGenerator = new RedrawObjectGenerator();
-        gameState = mock(GameState.class);
-        context = mock(Context.class);
-        jsonObject = new JSONObject();
-        jsonArray = new JSONArray();
-        cardGenerator = new CardGenerator(context);
-        cardList = new ArrayList<>();
     }
 
     @Test
     void testHalfList() {
         List<Card> newList = Arrays.asList(
-                new Card("Name 1", skellige, null, 8, fog, "name1", 1, "Time to look death in the face."),
-                new Card("Name 2", skellige, null, 2, muster, "name2", 3, "Text 2"),
-                new Card("Name 3", weather, null, 5, bond, "name3", 2, "Text 3"),
-                new Card("Name 4", special, null, 3, fog, "name4", 4, "Text 4")
+                new Card("Name 1", skellige, null, 8, fog, "name1.jpg", 1, "Time to look death in the face."),
+                new Card("Name 2", skellige, null, 2, muster, "name2.jpg", 3, "Text 2"),
+                new Card("Name 3", weather, null, 5, bond, "name3.jpg", 2, "Text 3"),
+                new Card("Name 4", special, null, 3, fog, "name4.jpg", 4, "Text 4")
                 );
 
         List<List<Card>> expectedResultListOfList = Arrays.asList(
