dea77e8bf856984dd5a3f67572888644a539bbd3
patzineubi <105806213+patzineubi@users.noreply.github.com>
2023-06-12 11:16:38 +0200

    fix: - code smells for RedrawDragListener

--- app/src/main/java/at/moritzmusel/gwent/ui/RedrawDragListener.java
+++ app/src/main/java/at/moritzmusel/gwent/ui/RedrawDragListener.java
@@ -10,11 +10,7 @@ public class RedrawDragListener implements View.OnDragListener {
 
     TextView redrawDrop;
     RedrawActivity mRedrawActivity;
-    int cnt = 0;
 
-    /**
-     * Constructor
-     */
     public RedrawDragListener(RedrawActivity activity, TextView target) {
         mRedrawActivity = activity;
         redrawDrop = target;
@@ -26,36 +22,11 @@ public class RedrawDragListener implements View.OnDragListener {
         int dragEvent = event.getAction();
         if (view.getId()== R.id.txtRedrawDrop) {
             switch (dragEvent) {
-                case DragEvent.ACTION_DRAG_ENTERED:
-                        //redrawDrop.setText(cnt + "/3");
-                    break;
-                case DragEvent.ACTION_DRAG_EXITED:
-                        //redrawDrop.setText("Hier ablegen");
-                    break;
                 case DragEvent.ACTION_DROP:
-                        //redrawDrop.setText("dropped");
-//                        if (cnt <= 2){
-                            mRedrawActivity.replaceCard((View) event.getLocalState());
-//                            cnt++;
-//                            mRedrawActivity.setRedrawCountView(cnt);
-//                        }if (cnt == 3){
-//                            redrawDrop.setText("Bitte auf Fertig tippen");
-//                        }
-                    //Toast.makeText(view.getContext(), "new card should appear", Toast.LENGTH_SHORT).show();
-                    break;
+                    mRedrawActivity.replaceCard((View) event.getLocalState()); break;
+                default: break;
             }
         }
-//        if (event.getAction() == DragEvent.ACTION_DROP) {
-//            isDropped = true;
-//            int positionTarget = -1;
-//
-//            View viewSource = (View) event.getLocalState();
-//            int viewId = view.getId();
-//
-//            if(viewId == R.id.txt_Redraw_Drop) {
-//                Toast.makeText(view.getContext(), "new Card should appear lol", Toast.LENGTH_SHORT).show();
-//            }
-//        }
         return true;
    }
 }
