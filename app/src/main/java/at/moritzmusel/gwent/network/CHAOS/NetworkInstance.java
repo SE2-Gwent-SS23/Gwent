cca15d524d78bfeded211e0a93dce32488c48a63
moritzmusel <mormusel@edu.aau.at>
2023-06-19 17:16:31 +0200

    removal of smells

--- app/src/main/java/at/moritzmusel/gwent/network/CHAOS/NetworkInstance.java
+++ app/src/main/java/at/moritzmusel/gwent/network/CHAOS/NetworkInstance.java
@@ -7,6 +7,7 @@ import com.google.android.gms.nearby.connection.ConnectionsClient;
 public class NetworkInstance {
     @SuppressLint("StaticFieldLeak")
     private static Network network;
+    private NetworkInstance(){}
 
     public static Network getInstance(ConnectionsClient connectionsClient, TriggerValueChangeListener onConnectionSuccessfulTrigger){
         if (network == null) network = new Network(connectionsClient, onConnectionSuccessfulTrigger);
